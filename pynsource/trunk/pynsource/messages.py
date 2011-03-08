5577de7e197eb132c70895c616ea18a4613e5315
abulka <abulka@6461a6f0-73df-cf69-53d8-f1e9cabed33a>
2011-03-08 07:18:16 +0000

    constants and messages moved to new messages.py

---
+++ pynsource/trunk/pynsource/messages.py
@@ -0,0 +1,35 @@
+WEB_VERSION_CHECK_URL = "http://www.atug.com/downloads/pynsource-latest.txt"
+WEB_PYNSOURCE_HOME_URL = "http://www.andypatterns.com/index.php/products/pynsource/"
+
+ABOUT_MSG = """
+PyNSource GUI
+
+Version %s
+
+A GUI front end to the python code scanner PyNSource that generates UML diagrams from Python Source code.
+
+(c) Andy Bulka 2004-2011
+http://www.andypatterns.com/index.php/products/pynsource/
+
+License: GPL 3 (free software).
+"""
+
+WEB_UPDATE_MSG = """
+There is a newer version of PyNSource GUI available:  %s
+
+%s
+
+Do you wish to visit the download page now?
+"""
+
+HELP_MSG = """
+PyNSource Gui Help:
+
+Import a python file and it will be reverse engineered and represented as UML.
+
+Import multiple files by multiple selecting files (hold ctrl and/or shift) in the file open dialog.
+You can import repeatedly and incoming classes will be added and wired up to existing classes on the workspace.
+
+Layout is a bit dodgy so arrange your layout a little and then do a screen grab (using your favourite screen grabbing tool) or print.
+You cannot add new classes in the GUI, this is just a reverse engineering tool.  You can however delete uncessesary classes by right clicking on the node.
+"""
--- pynsource/trunk/pynsource/pyNsourceGui.py
+++ pynsource/trunk/pynsource/pyNsourceGui.py
@@ -22,44 +22,10 @@ import wx
 import wx.lib.ogl as ogl
 from wx import Frame
 import os, stat
+from messages import *
 
 APP_VERSION = 1.51
-WEB_VERSION_CHECK_URL = "http://www.atug.com/downloads/pynsource-latest.txt"
-WEB_PYNSOURCE_HOME_URL = "http://www.andypatterns.com/index.php/products/pynsource/"
 WINDOW_SIZE = (640,480)
-
-ABOUT_MSG = """
-PyNSource GUI
-
-Version %s
-
-A GUI front end to the python code scanner PyNSource that generates UML diagrams from Python Source code.
-
-(c) Andy Bulka 2004-2011
-http://www.andypatterns.com/index.php/products/pynsource/
-
-License: GPL 3 (free software).
-""" % APP_VERSION
-
-WEB_UPDATE_MSG = """
-There is a newer version of PyNSource GUI available:  %s
-
-%s
-
-Do you wish to visit the download page now?
-"""
-
-HELP_MSG = """
-PyNSource Gui Help:
-
-   Import a python file and it will be reverse engineered and represented as UML.
-   
-   Import multiple files by multiple selecting files (hold ctrl and/or shift) in the file open dialog.
-   You can import repeatedly and incoming classes will be added and wired up to existing classes on the workspace.
-   
-   Layout is a bit dodgy so arrange your layout a little and then do a screen grab (using your favourite screen grabbing tool) or print.
-   You cannot add new classes in the GUI, this is just a reverse engineering tool.  You can however delete uncessesary classes by right clicking on the node.
-"""
 
 class DiamondShape(ogl.PolygonShape):
     def __init__(self, w=0.0, h=0.0):
@@ -930,7 +896,7 @@ class MainApp(wx.App):
 
 
     def OnAbout(self, event):
-        self.MessageBox(ABOUT_MSG.strip())
+        self.MessageBox(ABOUT_MSG.strip() %  APP_VERSION)
 
     def OnVisitWebsite(self, event):
         import webbrowser
