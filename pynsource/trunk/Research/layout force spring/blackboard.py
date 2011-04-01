590ead7844cd47e7a431bff301969735821ac1b7
abulka <abulka@6461a6f0-73df-cf69-53d8-f1e9cabed33a>
2011-04-01 23:44:10 +0000

    doco

--- pynsource/trunk/Research/layout force spring/blackboard.py
+++ pynsource/trunk/Research/layout force spring/blackboard.py
@@ -86,10 +86,13 @@ class LayoutBlackboard:
         is tangled. Scaling layout->world may have introduced these LL (or LL
         raw - ignoring nodes width/height) crossings.
         
-        This experiment: Want to test if LL raw stays the same true for all scales.
-        Turns out that it does not.  And it may start at non zero too (depending on the graph and the scale at which you start this experiment at).
+        This experiment: Want to test if LL raw stays the same true for all
+        scales. Turns out that it does not. And it may start at non zero too
+        (depending on the graph and the scale at which you start this experiment
+        at).
 
-        We don't do any overlap removal here of course - cos this may introduce LLs
+        We don't do any overlap removal here of course - cos this may
+        introduce/remove LLs
         """
         MAX_SCALE = 1.4
         SCALE_STEP = 0.2
@@ -158,6 +161,7 @@ class LayoutBlackboard:
 
         strategy = ":reduce pre overlap removal NN overlaps"
         strategy = ":reduce post overlap removal LN crossings"
+        strategy = ":reduce post overlap removal LN and LL crossings":
         
         Operates repeatedly on the layout coords, so ignores anything you
         have done since, like world coord overlap removal.
@@ -165,40 +169,48 @@ class LayoutBlackboard:
         Running overlap removal at different scales can remove or introduce
         LL and LN crossings (but by definition, no NN overlaps ;-)
         
-        After AllToWorldCoords() we are getting a picture of the pure layout result
+        After AllToWorldCoords() we are getting a picture of the pure layout
+        result, scaled up
         
-        Calling self.coordmapper.Recalibrate(scale) before AllToWorldCoords() simply
-        expands or contracts the appearance of the world view nodes
+        Calling self.coordmapper.Recalibrate(scale) before AllToWorldCoords()
+        simply expands or contracts the appearance of the world view nodes
         
-        After AllToWorldCoords(), looking at pure layout result there will typically be
-         -- LL == 0, unless the spring layout couldn't untangle itself, that is.
-         *- NN many, scaling up helps reduce (running overlap remover removes totally)
+        After AllToWorldCoords(), looking at pre NN overlap removal, layout
+        result there will typically be
+         -- LL some, act of translating to world introduces them.  More if the spring layout couldn't untangle itself.
+         *- NN many, scaling up helps reduce (of course running overlap remover removes totally)
          -- LN many, scaling up helps reduce (running overlap remover may make it better or worse)
         
         After RemoveOverlaps()
-         *- LL == 0
+         *- LL some
          -- NN == 0, unless algorithm failed
          *- LN some
          
-         Note: * indicates the things we are looping and testing
+         Note: * indicates the things we are looping and testing for. Mainly
+         post overlap removal information - obviously since that is the 'look'
+         that we are trying to make look nice, but we also look at pre overlap
+         removal NN overlaps, which gives us some indication of how crowded we
+         are and thus how far away from pure spring layout our graph will look
+         after an overlap removal. We want to stay looking like the spring
+         layout and not shove nodes all over the place. Possibly another way of
+         guaging how much overlap removal has corrupted our look, would be to
+         calc how much nodes have moved out of position after an overlap removal
+         run - don't know.
         """
-        NODE_NODE = 3
+        ACCEPTABLE_NODE_NODE_PRE_REMOVAL = 3
         MAX_SCALE = 1.4
         SCALE_STEP = 0.2
         SCALE_START = 3.2
         
         self.controller.coordmapper.Recalibrate(scale=SCALE_START)
         for i in range(15):
-            num_line_line_crossings, num_node_node_overlaps, num_line_node_crossings = \
-                self.GetVitalStats(scale=self.controller.coordmapper.scale - SCALE_STEP, animate=animate)
-
-            if num_node_node_overlaps == None and num_line_node_crossings == None:
-                print "Mad: Aborting - no point since found %d tangled, post spring layout LL crossings!" % num_line_line_crossings
-                break
+            res = self.GetVitalStats(scale=self.controller.coordmapper.scale - SCALE_STEP, animate=animate)
             
+            num_line_line_crossings, num_node_node_overlaps, num_line_node_crossings = res
+
             if strategy == ":reduce pre overlap removal NN overlaps":
-                if num_node_node_overlaps <= NODE_NODE:
-                    print "Mad: Aborting expansion since num NN overlaps <= %d" % NODE_NODE
+                if num_node_node_overlaps <= ACCEPTABLE_NODE_NODE_PRE_REMOVAL:
+                    print "Mad: Aborting expansion since num NN overlaps <= %d" % ACCEPTABLE_NODE_NODE_PRE_REMOVAL
                     break
             elif strategy == ":reduce post overlap removal LN crossings":
                 if num_line_node_crossings == 0:
@@ -212,22 +224,20 @@ class LayoutBlackboard:
                 assert False, "Mad: unknown strategy"
     
             if self.controller.coordmapper.scale < MAX_SCALE:
-                print "Mad: Aborting expansion - gone too far."
+                print "Mad: Aborting expansion - gone too far.", self.controller.coordmapper.scale
                 break
     
-        print "Mad: End - scale ended up as ", self.controller.coordmapper.scale
-
         if animate:
             self.controller.stateofthenation()
-        #self.stage2() # does overlap removal and stateofthenation
         
         return num_line_line_crossings, num_node_node_overlaps, num_line_node_crossings
 
     def GetVitalStats(self, scale, animate=False):
         """
         No layout performed, assuming we are working off juicy post layout coords
-        Expand directly to the original scale, and calc vitals stats
+        Expand directly to the 'scale', and calc vitals stats
         Same as ScaleUpMadly except only one scale made
+        NN Overlap Removal performed.
         """
         
         self.controller.coordmapper.Recalibrate(scale=scale)
@@ -238,15 +248,16 @@ class LayoutBlackboard:
         
         """Pre Overlap Removal"""
         
-        # see how many INITIAL, PRE OVERLAP REMOVAL line-line crossings there are.
-        num_line_line_crossings = len(self.graph.CountLineOverLineIntersections())
-        if num_line_line_crossings > 0:
-            return num_line_line_crossings, None, None      # graph is tangled, don't proceed
+        # The only thing of interest here pre node overlap removal are the
+        # initial NN as this gives us some indication of how crowded we are and
+        # how hard the node overlap removal algorithm has to work. If too much
+        # overlap removal is done it detracts from the nice layout that was
+        # worked out
 
-        # see how many INITIAL, PRE OVERLAP REMOVAL node-node overlaps the expansion fixes
+        # count NN (pre overlap removal)
         num_node_node_overlaps = self.controller.overlap_remover.CountOverlaps()
 
-        """Remove Overlaps (NN)"""
+        """Remove Node Overlaps"""
         self.controller.overlap_remover.RemoveOverlaps(watch_removals=False)
 
         """Post Overlap Removal"""
@@ -257,7 +268,7 @@ class LayoutBlackboard:
         # How many LL reduced (or perhaps increased) after expansion & post removing NN overlaps
         num_line_line_crossings = len(self.graph.CountLineOverLineIntersections())
 
-        print "Mad: At scale %.1f LL %d NN %d LN %d" % (self.controller.coordmapper.scale, num_line_line_crossings, num_node_node_overlaps, num_line_node_crossings)
+        print "GetVitalStats: At scale %.1f NN_pre %d LN %d LL %d" % (self.controller.coordmapper.scale, num_node_node_overlaps, num_line_node_crossings, num_line_line_crossings)
     
         return num_line_line_crossings, num_node_node_overlaps, num_line_node_crossings
 
