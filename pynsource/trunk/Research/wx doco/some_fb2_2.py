33800dc91b0786092d64779eecab4bf89f568acf
abulka <abulka@6461a6f0-73df-cf69-53d8-f1e9cabed33a>
2011-04-21 07:54:59 +0000

    Discovered FormBuilder gui builder.

---
+++ pynsource/trunk/Research/wx doco/some_fb2_2.py
@@ -0,0 +1,9 @@
+import wx
+from some_fb2_gen import MyFrame2
+
+app = wx.App()
+frame = MyFrame2(None)
+frame.m_customControl1.SetText(open(r'..\..\pynsource\printframework.py').read())
+frame.m_customControl1.EmptyUndoBuffer()
+frame.Show()
+app.MainLoop()
