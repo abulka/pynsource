9cc9404fe17030fb7640e1319fdb5f3cdb79e877
abulka <abulka@6461a6f0-73df-cf69-53d8-f1e9cabed33a>
2012-02-15 07:38:25 +0000

    Created windows build script using bb-freeze tool/egg.

---
+++ pynsource/trunk/buildwin.bat
@@ -0,0 +1,23 @@
+@echo off
+REM easy_install bbfreeze
+REM ensure "c:\Program Files\7-Zip" or (x86) version is in your path
+
+set outsourcezip=pyNsource-1.6-src.zip
+set outstandalonezip=pyNsource-1.6-standalone.zip
+
+svn info > build.txt
+
+REM goto step3a
+
+REM buildstandalone exe zip
+cd pynsource
+del /q dist\*.*
+del /q build\*.*
+REM python setup.py py2exe
+bb-freeze pyNsourceGui.py
+cd ..
+
+:step3a
+7z a -tzip  dist\%outstandalonezip% .\pynsource\dist\* .\Readme.txt .\build.txt
+
+
