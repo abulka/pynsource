826da258fe5f1dfdeb165922b9656c00f97c8723
abulka <abulka@6461a6f0-73df-cf69-53d8-f1e9cabed33a>
2012-03-12 13:11:32 +0000

    hexmvc - REST api design

--- pynsource/trunk/Research/hexmvc/architecture3/ServerBottleAdapter.py
+++ pynsource/trunk/Research/hexmvc/architecture3/ServerBottleAdapter.py
@@ -46,10 +46,62 @@ class Server(object):
             self.observers.CMD_ADD_THING("fred")
             return 'The model length is now %d' % self.model.size
 
-        @route('/json')
-        def json():
+        # REST API
+
+        @route('/jsontest')
+        def jsontest():
             import time
             return {'status':'online', 'servertime':time.time()}
     
+        """
+        REST API DESIGN
+
+        Need to expose some sort of referencing scheme to refer to thing objects.
+            In Oo version the memory address? Do we need to actually create an integer id for the oo version?
+            In Sql version the db id?
+        
+        GET /things
+            Example:
+                GET http://localhost:8081/things
+            Comment:
+                like dumpthings except offers list of (id, link)
+            XML:
+                <thinglink info="blah" href="http://localhost:8081/things/1"/>
+                <thinglink info="blah2" href="http://localhost:8081/things/2"/>
+
+        GET /things/id
+            Example:
+                GET http://localhost:8081/things/1
+            Comment:
+                display just the thing object. Should we include the id in the xml or
+                the fact that its on the url enough?
+            XML:
+                <thing id="1">thing info content here</thing>
+            JSON:
+                {'id':"1", 'info':"thing info content here"}
+
+        POST /things, thing
+            Example:
+                POST http://localhost:8081/things, <thing>thing2 info content here</thing>
+            Comment:
+                Adds a thing.  We get a link back e.g. http://localhost:8081/things/2
+
+        PUT /things/id, thing
+            Example:
+                POST http://localhost:8081/things/1, <thing>thing MODIFIED info content here</thing>
+            Comment:
+                Modifes a thing.
+                
+        DELETE /things/id
+            Example:
+                DELETE http://localhost:8081/things/1
+            Comment:
+                Deletes a thing.
+                
+        P.S. Useful Chrome plugins:
+            Simple REST Client
+            JSON formatter
+        """
+        
         run(host=self.host, port=self.port)
         # nothing runs after this point
