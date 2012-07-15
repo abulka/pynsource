a700cbf7ea43d6c11b33b870551b21a60bad494f
abulka@gmail.com <abulka@gmail.com@6461a6f0-73df-cf69-53d8-f1e9cabed33a>
2012-07-15 09:23:13 +0000

    Added more insertion commands.
    Update issue 12.
    More insertion commands created - these still delegate out to other methods which are command like, thus the next step is to remove those methods and use the command objects soley.

--- src/app/cmds/insertion.py
+++ src/app/cmds/insertion.py
@@ -1,15 +1,68 @@
 from base_cmd import CmdBase
+import wx
 
 class CmdInsertNewComment(CmdBase):
-    """ Delete nodes """
+    """ Insert node """
 
     def execute(self):
-        """ Docstring """
+        """ insert comment node """
         assert self.umlwin
         self.umlwin.CmdInsertNewComment()
             
     def undo(self):  # override
-        """ Docstring """
+        """ undo insert new comment """
         # not implemented
 
+class CmdInsertNewNode(CmdBase):
+    """ Insert new node """
+
+    def __init__(self, umlwin, wxapp):
+        """ pass in all the relevant context """
+        super(CmdInsertNewNode, self).__init__(umlwin)
+        self.wxapp = wxapp
+
+    def execute(self):
+        """ insert the new node and refresh the ascii tab too """
+        assert self.umlwin
+        assert self.wxapp
+        self.umlwin.CmdInsertNewNode()
+        self.wxapp.RefreshAsciiUmlTab()
+            
+    def undo(self):  # override
+        """ undo insert new node """
+        # not implemented
+
+
+class CmdInsertImage(CmdBase):
+    """ Insert image """
+
+    def __init__(self, umlwin, frame, config):
+        """ pass in all the relevant context """
+        super(CmdInsertImage, self).__init__(umlwin)
+        self.config = config
+        self.frame = frame
+
+    def execute(self):
+        """ Docstring """
+        assert self.umlwin
+        assert self.config
+
+        filename = None
         
+        thisdir = self.config.get('LastDirInsertImage', '.') # remember dir path
+        dlg = wx.FileDialog(parent=self.frame, message="choose", defaultDir=thisdir,
+            defaultFile="", wildcard="*.jpg", style=wx.OPEN, pos=wx.DefaultPosition)
+        if dlg.ShowModal() == wx.ID_OK:
+            filename = dlg.GetPath()
+
+            self.config['LastDirInsertImage'] = dlg.GetDirectory()  # remember dir path
+            self.config.write()
+        dlg.Destroy()
+        
+        self.umlwin.CmdInsertNewImageNode(filename)
+            
+    def undo(self):  # override
+        """ Docstring """
+        # not implemented
+        
+
