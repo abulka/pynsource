d4e9b4df3673908c86afb22e57a7b52493d779b9
abulka@gmail.com <abulka@gmail.com@6461a6f0-73df-cf69-53d8-f1e9cabed33a>
2012-08-13 12:32:51 +0000

    ast parse_and_convert - organising comments

--- Research/parsing/parseoldnew.py
+++ Research/parsing/parseoldnew.py
@@ -151,28 +151,38 @@ def convert_ast_to_old_parser(node, filename):
         """
         Usage:
             is_rhs_reference_to_a_class()
+                Returns t/f
+                Rsulting reference in .rhs_ref_to_class
+                
+        Background:
+            You get here if
+                self.made_assignment OR
+                self.made_append_call    ( you can't have these both be true at the same time )
+            and if
+                self.lhs[0] == 'self' AND
+                len(self.rhs) > 0:
             
         Scenarios:
-            ... = Blah()      want name - before first call which is Blah
-            ...append(Blah()) want name - before first call which is Blah (remember the append is on the lhs not the rhs)
+            ... = Blah()                                want name - before first call which is Blah
+            ...append(Blah())                           want name - before first call which is Blah (remember the append is on the lhs not the rhs)
             
-            ... = blah        may be reinterpreted as      = Blah()  if Blah class found - a relaxed rule I admit
-            ...append(blah)   may be reinterpreted as append(Blah()) if Blah class found - a relaxed rule I admit
+            ... = blah                                  may be reinterpreted as      = Blah()  if Blah class found - a relaxed rule I admit
+            ...append(blah)                             may be reinterpreted as append(Blah()) if Blah class found - a relaxed rule I admit
             
-            ... = 10          won't get here because no rhs
-            ...append(10)     won't get here because no rhs
+            ... = 10                                    won't get here because no rhs
+            ...append(10)                               won't get here because no rhs
 
             new cases just coming in!
             -------------------------
             * rule seems to be get name or if the name has attributes, the
               name's last attr before the first call (if there is a call)
             
-            ... = a.Blah()                                      want names's last attr - before first call
-            ... = a.blah                                        want names's last attr - no call here 
-            self._max_items = CommandManager.MaxListSize        want names's last attr - no call here - no rhs class ref here !!
-            
-            ... = a.Blah(b.Fred())                              want names's last attr - before first call
-            self.flageditor = FlagEditor(gamestatusstate=self)  want name              - before first call
+            ... = a.Blah()                              want names's last attr - before first call
+            ... = a.blah                                want names's last attr - no call here 
+            ..append(a.blah)                            look inside append - want names's last attr - no call here 
+            ... = Blah.MaxListSize                      want names's last attr - no call here - no rhs class ref here !!
+            ... = a.Blah(b.Fred())                      want names's last attr - before first call
+            ... = Blah(b=self)                          want name              - before first call
             
             more new cases just coming in!
             ------------------------------
@@ -181,41 +191,45 @@ def convert_ast_to_old_parser(node, filename):
             Also if there are subsequent . calls after the class candidate
             disqualify
             
-            self.curr.append(" "*self.curr_width)               skip - want names's last attr - no call here - curr_width has no corresponding class
-            self.canvas = self.umlwin.GetDiagram().GetCanvas()  skip - would be too relaxed to interpret GetDiagram as a class.
-                                                                    especially since there are further . calls
-                                                                    especially since self.umlwin is not an import
-            = a.GetDiagram()                                    skip - unless a is an import
-            = a.b.GetDiagram()                                  skip - unless a.b is an import
-            = self.GetDiagram()                                 skip - unless self is an import
-            = self.a.GetDiagram()                               skip - unless self.a is an import
+            ...append(" "*self.curr_width)              skip - want names's last attr - no call here - curr_width has no corresponding class
+            ... = self.umlwin.GetDiagram().GetCanvas()  skip - would be too relaxed to interpret GetDiagram as a class.
+                                                                especially since there are further . calls
+                                                                especially since self.umlwin is not an import
+            ... = a.GetDiagram()                        skip - unless a is an import
+            ... = a.b.GetDiagram()                      skip - unless a.b is an import
+            ... = self.GetDiagram()                     skip - unless self is an import
+            ... = self.a.GetDiagram()                   skip - unless self.a is an import
 
         """
 
         def __init__(self, visitor):
-            self.visitor = visitor
+            self.v = visitor
             
-            self.rhs = visitor.rhs
-            assert len(self.rhs) > 0
+            assert len(self.v.rhs) > 0
+            assert not (self.v.made_assignment and self.v.made_append_call) # these both can't be true
             
+            self.rhs_ref_to_class = None
             self._calc_rhs_ref_to_class()
             
         def is_rhs_reference_to_a_class(self):
             return self._relaxed_is_instance_a_known_class() or self._is_class_creation()
 
         def _calc_rhs_ref_to_class(self):
-            if self.visitor.made_rhs_call:
-                pos = self.visitor.pos_rhs_call_pre_first_bracket
-                self.rhs_ref_to_class = self.rhs[pos]      # want names's last attr - before first call
+            if self.v.made_rhs_call: # want names's last attr - before first call
+                
+                pos = self.v.pos_rhs_call_pre_first_bracket
+                self.rhs_ref_to_class = self.v.rhs[pos]      
                 
                 # adjust for import syntax   TODO do this later?  TODO do this as part of FULL import chain checking logic
-                if pos > 0 and self.rhs[pos-1] in self.visitor.imports_encountered:
-                    self.rhs_ref_to_class = "%s.%s" % (self.rhs[pos-1], self.rhs[pos])
-            else:
-                self.rhs_ref_to_class = self.rhs[-1]     # want names's last attr - no call here 
+                if pos > 0 and self.v.rhs[pos-1] in self.v.imports_encountered:
+                    self.rhs_ref_to_class = "%s.%s" % (self.v.rhs[pos-1], self.v.rhs[pos])
+                    
+            else: # must be an append, we want the thing inside the append statement
+                
+                self.rhs_ref_to_class = self.v.rhs[-1]     # want names's last attr - no call here 
             
         def _relaxed_is_instance_a_known_class(self):
-            for c in self.visitor.quick_parse.quick_found_classes:
+            for c in self.v.quick_parse.quick_found_classes:
                 if c.lower() == self.rhs_ref_to_class.lower():
                     self.rhs_ref_to_class = c  # transform into proper class name not the instance
                     return True
@@ -230,9 +244,9 @@ def convert_ast_to_old_parser(node, filename):
         def _is_class_creation(self):            
             # Make sure the rhs is a class creation call NOT a function call.
             t = self.rhs_ref_to_class
-            return self.visitor.made_rhs_call and \
+            return self.v.made_rhs_call and \
                 t not in pythonbuiltinfunctions and \
-                t not in self.visitor.model.modulemethods
+                t not in self.v.model.modulemethods
             
     class Visitor(ast.NodeVisitor):
         
