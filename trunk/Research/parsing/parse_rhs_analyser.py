b3422bbdcc65b03a42ffb97d1b9b1c2a66469178
abulka <abulka@6461a6f0-73df-cf69-53d8-f1e9cabed33a>
2012-08-16 07:31:15 +0000

    RhsAnalyser - refactored the comments and method order.

--- trunk/Research/parsing/parse_rhs_analyser.py
+++ trunk/Research/parsing/parse_rhs_analyser.py
@@ -7,9 +7,15 @@ from keywords import pythonbuiltinfunctions
 class RhsAnalyser:
     """
     Usage:
-        is_rhs_reference_to_a_class()
-            Returns t/f
-            Rsulting reference in .rhs_ref_to_class
+        ra.is_rhs_reference_to_a_class()
+
+        Returns t/f
+        Resulting probably reference to class is in ra.rhs_ref_to_class
+            
+    Example:
+        ra = RhsAnalyser(v)
+        result = ra.is_rhs_reference_to_a_class()
+        print ra.rhs_ref_to_class
             
     Background:
         You get here if
@@ -20,43 +26,61 @@ class RhsAnalyser:
             len(self.rhs) > 0:
         
     Scenarios:
-        ... = Blah()                                want name - before first call which is Blah
-        ...append(Blah())                           want name - before first call which is Blah (remember the append is on the lhs not the rhs)
+        Note there are use cases A-G with sub tests 1...5 and I use the syntax
+        e.g. #B2 to refer to Test Class B test method 2.
         
-        ... = blah                                  may be reinterpreted as      = Blah()  if Blah class found - a relaxed rule I admit
-        ...append(blah)                             may be reinterpreted as append(Blah()) if Blah class found - a relaxed rule I admit
-        
-        ... = 10                                    won't get here because no rhs
-        ...append(10)                               won't get here because no rhs
+        The shorthand / domain specific language syntax "where: class Blah,
+        import a - T a.Blah" means assuming there exists a class 'Blah' found in
+        the quick find list (supplied by visitor object), and 'a' is found in
+        the lists of imports (supplied by visitor object) the result should be
+        True with a ra.rhs_ref_to_class of a.Blah
 
-        new cases just coming in!
-        -------------------------
-        * rule seems to be get name or if the name has attributes, the
-          name's last attr before the first call (if there is a call)
-        
-        ... = a.Blah()                              want names's last attr - before first call
-        ... = a.blah                                want names's last attr - no call here 
-        ..append(a.blah)                            look inside append - want names's last attr - no call here 
-        ... = Blah.MaxListSize                      want names's last attr - no call here - no rhs class ref here !!
-        ... = a.Blah(b.Fred())                      want names's last attr - before first call
-        ... = Blah(b=self)                          want name              - before first call
-        
-        more new cases just coming in!
-        ------------------------------
-        * rule seems to be to identify the . call chain before
-        the class candidate and ensure they are in the imports
-        Also if there are subsequent . calls after the class candidate
-        disqualify
-        
-        ...append(" "*self.curr_width)              skip - want names's last attr - no call here - curr_width has no corresponding class
-        ... = self.umlwin.GetDiagram().GetCanvas()  skip - would be too relaxed to interpret GetDiagram as a class.
-                                                            especially since there are further . calls
-                                                            especially since self.umlwin is not an import
-        ... = a.GetDiagram()                        skip - unless a is an import
-        ... = a.b.GetDiagram()                      skip - unless a.b is an import
-        ... = self.GetDiagram()                     skip - unless self is an import
-        ... = self.a.GetDiagram()                   skip - unless self.a is an import
-
+        INPUT:
+        rhs call
+        or append   OUTPUT
+        expression  token       Logic and notes
+        ==========|===========|=======================
+     A  Blah()      Blah        1  if Blah class exists and its NOT in module methods  *2
+                    None        2  if Blah class does NOT exist and it IS in module methods
+                    Blah        3  if Blah class exists and it IS in module methods - CONTRADICATION, assume Blah is class
+                    Blah        4  if Blah class does NOT exist and its NOT in module methods - GUESS yes, if starts with uppercase letter
+                    
+     B  blah        Blah        1  if Blah class exists and blah is NOT in module methods ==> (token transmografied)
+                    None        2  if Blah class exists and blah IS in module methods (clear intent that blah is a function ref)
+                    Blah        3  if Blah class exists and blah is NOT in module methods but Blah IS in module methods
+                                    (if Blah is a function this is unrelated to blah instance)  ==> (token transmografied)
+                    None        4  if Blah class doesn't exist
+                    None        5  if blah class exists - syntax is just plain wrong for class creation.
+                                *  Other cases - see note *3
+                    
+     C  a.Blah()    a.Blah      1 where: class Blah, import a - T a.Blah
+                    a.Blah      2 where: import a - T a.Blah   Most common case, e.g. self.popupmenu = wx.Menu() where all you know about is that you imported wx.
+                    None        3 where: class Blah - F
+                    None        4 where: - F
+            
+                    
+     D  a.b.Blah()  a.b.Blah    if Blah class exists and imported a.b
+                    a.b.Blah    if NO Blah class exists and imported a.b   Most common case, see above #C2
+                    None        no import of a.b exists
+                    
+     E  a().Blah()  None        too tricky
+     F  a().blah    None        too tricky
+     G  a.blah      None        too tricky  *1
+    
+        *1 First of all, whether or not 'blah' class exists, syntax is just plain
+            wrong for class creation. Presence of preceding a. doesn't make a
+            difference.
+            
+           Secondly, sure we could potentially check for either
+                import a.Blah
+                from a import Blah
+            and transmogrify, but its all a bit vague, safer to just return F/None.
+    
+        *2 Note we used to be that unless its a module method
+           or built in method, then assume its a class. Now logic is a bit smarter:
+           
+        *3 Note if class 'blah' exists instead of class 'Blah'
+            then is just classic case A use cases.
     """
 
     def __init__(self, visitor):
@@ -66,75 +90,9 @@ class RhsAnalyser:
         assert not (self.v.made_assignment and self.v.made_append_call) # these both can't be true
         
         self.rhs_ref_to_class = None
-        self._calc_rhs_ref_to_class()
+        self.calc_rhs_ref_to_class()
     
-    @property
-    def pos(self):
-        return self.v.pos_rhs_call_pre_first_bracket
-    @property
-    def prefix(self):
-        return ".".join(self.v.rhs[0:self.pos])
-
-    def is_prefixed_class_call(self):
-        return self.v.made_rhs_call and self.pos > 0
-
-    def is_syntax_after_class_call(self):
-        lastpos = len(self.v.rhs) - 1
-        return self.v.made_rhs_call and lastpos > self.pos
-
-    def is_prefixed_instance(self):
-        return not self.v.made_rhs_call and len(self.v.rhs) > 1
-        
-    def is_rhs_reference_to_a_class(self):
-        
-        def rhsbrackets():
-            return self.v.made_rhs_call and self.v.pos_rhs_call_pre_first_bracket == 0
-
-        if self.is_prefixed_instance(): #G
-            self.rhs_ref_to_class = None
-            return False
-
-        if self.v.made_rhs_call and self.is_syntax_after_class_call():  #E, #F, #G
-            self.rhs_ref_to_class = None
-            return False
-            
-        if self.is_prefixed_class_call(): #C, #D
-            if self.prefix in self.v.imports_encountered: # C1, # D4
-                self.rhs_ref_to_class = "%s.%s" % (self.prefix, self.v.rhs[self.pos])
-                return True
-            else:
-                self.rhs_ref_to_class = None
-                return False
-            
-        if self.class_exists() and not self.in_module_methods_etc() and rhsbrackets(): # A1
-            return True
-        
-        if self.class_exists(instance_check=True) and not self.in_module_methods_etc() and not rhsbrackets(): # B1
-            self.rhs_ref_to_class = self.upper_just_first_char()
-            return True
-        
-        if not self.class_exists() and self.in_module_methods_etc() and rhsbrackets(): # A2
-            self.rhs_ref_to_class = None
-            return False
-        
-        if self.class_exists(instance_check=True) and self.in_module_methods_etc() and not rhsbrackets(): # B2
-            self.rhs_ref_to_class = None
-            return False
-
-        if self.class_exists() and self.in_module_methods_etc() and rhsbrackets(): # A3
-            return True
-        
-        if not self.class_exists() and not self.in_module_methods_etc() and rhsbrackets(): # A4
-            if self.rhs_ref_to_class[0].isupper():  # GUESS yes, if starts with uppercase letter
-                return True
-            else:
-                self.rhs_ref_to_class = None
-                return False
-                
-        self.rhs_ref_to_class = None
-        return False
-                
-    def _calc_rhs_ref_to_class(self):
+    def calc_rhs_ref_to_class(self):
         """
         IF made_rhs_call then one or more calls () involved on rhs (which would
         have been either function calls or new class instance creations) thus
@@ -162,6 +120,81 @@ class RhsAnalyser:
     def upper_just_first_char(self):
         return self.rhs_ref_to_class[0].upper() + self.rhs_ref_to_class[1:]
 
+    @property
+    def pos(self):
+        return self.v.pos_rhs_call_pre_first_bracket
+    @property
+    def prefix(self):
+        return ".".join(self.v.rhs[0:self.pos])
+
+    def is_prefixed_class_call(self):
+        return self.v.made_rhs_call and self.pos > 0
+
+    def is_prefixed_instance(self):
+        return not self.v.made_rhs_call and len(self.v.rhs) > 1
+
+    def is_there_syntax_after_class_call(self):
+        lastpos = len(self.v.rhs) - 1
+        return self.v.made_rhs_call and lastpos > self.pos
+
+    def is_rhs_reference_to_a_class(self):
+        """
+        This is the main API method to call.
+        Returns T/F
+        """
+        
+        if self.is_prefixed_instance(): #G
+            self.rhs_ref_to_class = None
+            return False
+
+        if self.v.made_rhs_call and self.is_there_syntax_after_class_call():  #E, #F, #G
+            self.rhs_ref_to_class = None
+            return False
+            
+        if self.is_prefixed_class_call(): #C, #D
+            if self.prefix in self.v.imports_encountered: # C1, # D4
+                self.rhs_ref_to_class = "%s.%s" % (self.prefix, self.v.rhs[self.pos])
+                return True
+            else:
+                self.rhs_ref_to_class = None
+                return False
+        
+        assert not self.is_prefixed_class_call()
+        
+        if self.class_exists() and not self.in_module_methods_etc() and self.v.made_rhs_call: # A1
+            return True
+        
+        if self.class_exists(instance_check=True) and not self.in_module_methods_etc() and not self.v.made_rhs_call: # B1
+            self.rhs_ref_to_class = self.upper_just_first_char()
+            return True
+        
+        if not self.class_exists() and self.in_module_methods_etc() and self.v.made_rhs_call: # A2
+            self.rhs_ref_to_class = None
+            return False
+        
+        if self.class_exists(instance_check=True) and self.in_module_methods_etc() and not self.v.made_rhs_call: # B2
+            self.rhs_ref_to_class = None
+            return False
+
+        if self.class_exists() and self.in_module_methods_etc() and self.v.made_rhs_call: # A3
+            return True
+        
+        if not self.class_exists() and not self.in_module_methods_etc() and self.v.made_rhs_call: # A4
+            if self.rhs_ref_to_class[0].isupper():  # GUESS yes, if starts with uppercase letter
+                return True
+            else:
+                self.rhs_ref_to_class = None
+                return False
+                
+        self.rhs_ref_to_class = None
+        return False
+
+               
+"""
+
+ TEST - BASE CLASS
+ 
+"""
             
 import unittest
 import sys
@@ -233,56 +266,11 @@ class TestCaseBase(unittest.TestCase):
             self.assertEqual(ra.is_rhs_reference_to_a_class(), result_should_be)
             self.assertEqual(ra.rhs_ref_to_class,              rhs_ref_to_class_should_be)
 
+
 """
-    RhsAnalyser Summary of Logic
-    ----------------------------
-    
-    INPUT:
-    rhs call
-    or append   OUTPUT
-    expression  token       Logic and notes
-    ==========|===========|=======================
- A  Blah()      Blah        1  if Blah class exists and its NOT in module methods  *2
-                None        2  if Blah class does NOT exist and it IS in module methods
-                Blah        3  if Blah class exists and it IS in module methods - CONTRADICATION, assume Blah is class
-                Blah        4  if Blah class does NOT exist and its NOT in module methods - GUESS yes, if starts with uppercase letter
-                
- B  blah        Blah        1  if Blah class exists and blah is NOT in module methods ==> (token transmografied)
-                None        2  if Blah class exists and blah IS in module methods (clear intent that blah is a function ref)
-                Blah        3  if Blah class exists and blah is NOT in module methods but Blah IS in module methods
-                                (if Blah is a function this is unrelated to blah instance)  ==> (token transmografied)
-                None        4  if Blah class doesn't exist
-                None        5  if blah class exists - syntax is just plain wrong for class creation.
-                            *  Other cases - see note *3
-                
- C  a.Blah()    a.Blah      1 where: class Blah, import a - T a.Blah
-                a.Blah      2 where: import a - T a.Blah   Most common case, e.g. self.popupmenu = wx.Menu() where all you know about is that you imported wx.
-                None        3 where: class Blah - F
-                None        4 where: - F
-        
-                
- D  a.b.Blah()  a.b.Blah    if Blah class exists and imported a.b
-                a.b.Blah    if NO Blah class exists and imported a.b   Most common case, see above #C2
-                None        no import of a.b exists
-                
- E  a().Blah()  None        too tricky
- F  a().blah    None        too tricky
- G  a.blah      None        too tricky  *1
 
-    *1 First of all, whether or not 'blah' class exists, syntax is just plain
-        wrong for class creation. Presence of preceding a. doesn't make a
-        difference.
-        
-       Secondly, sure we could potentially check for either
-            import a.Blah
-            from a import Blah
-        and transmogrify, but its all a bit vague, safer to just return F/None.
-
-    *2 Note we used to be that unless its a module method
-       or built in method, then assume its a class. Now logic is a bit smarter:
-       
-    *3 Note if class 'blah' exists instead of class 'Blah'
-        then is just classic case A use cases.
+ TESTS
+ 
 """
 
 class TestCase_A_Classic(TestCaseBase):
@@ -538,16 +526,16 @@ class TestCase_AddHoc(TestCaseBase):
                 result_should_be=True, rhs_ref_to_class_should_be='FlagEditor')
     
 def suite():
-    #suite1 = unittest.makeSuite(TestCase_A_Classic, 'test')
-    #suite2 = unittest.makeSuite(TestCase_B_RhsIsInstance, 'test')
-    #suite3 = unittest.makeSuite(TestCase_C_AttrBeforeClassic, 'test')
-    #suite4 = unittest.makeSuite(TestCase_D_MultipleAttrBeforeClassic, 'test')
-    #suite5 = unittest.makeSuite(TestCase_E_DoubleCall, 'test')
-    #suite6 = unittest.makeSuite(TestCase_F_CallThenTrailingInstance, 'test')
-    #suite7 = unittest.makeSuite(TestCase_G_AttrBeforeRhsInstance, 'test')
+    suite1 = unittest.makeSuite(TestCase_A_Classic, 'test')
+    suite2 = unittest.makeSuite(TestCase_B_RhsIsInstance, 'test')
+    suite3 = unittest.makeSuite(TestCase_C_AttrBeforeClassic, 'test')
+    suite4 = unittest.makeSuite(TestCase_D_MultipleAttrBeforeClassic, 'test')
+    suite5 = unittest.makeSuite(TestCase_E_DoubleCall, 'test')
+    suite6 = unittest.makeSuite(TestCase_F_CallThenTrailingInstance, 'test')
+    suite7 = unittest.makeSuite(TestCase_G_AttrBeforeRhsInstance, 'test')
     suite8 = unittest.makeSuite(TestCase_AddHoc, 'test')
-    #alltests = unittest.TestSuite((suite1, suite2, suite3, suite4, suite5, suite6, suite7))
-    alltests = unittest.TestSuite((suite8, ))
+    alltests = unittest.TestSuite((suite1, suite2, suite3, suite4, suite5, suite6, suite7))
+    #alltests = unittest.TestSuite((suite8, ))
     #alltests = unittest.TestSuite((suite3, suite4))
     return alltests
 
