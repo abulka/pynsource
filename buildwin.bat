8849d3820934a5237fd42f11f9af58a9ab4eaa73
abulka <abulka@6461a6f0-73df-cf69-53d8-f1e9cabed33a>
2012-08-23 02:04:17 +0000

    setup.py REMOVED from pyNsource (again) as am not using py2exe anymore for main exe building under windows.  __init__.py removed from pyNsource/src as that is not a package. buildwin.bat script cleaned up and old windows build all script deleted.
    Update issue 6
    setup.py REMOVED from pyNsource (again) as am not using py2exe anymore for main exe building under windows.  __init__.py removed from pyNsource/src as that is not a package. buildwin.bat script cleaned up and old windows build all script deleted.

--- buildwin.bat
+++ buildwin.bat
@@ -1,23 +1,36 @@
 @echo off
 REM easy_install bbfreeze
 REM ensure "c:\Program Files\7-Zip" or (x86) version is in your path
+REM ensure "c:\Program Files (x86)\Inno Setup 5\" is in your path
 
-set outsourcezip=pyNsource-1.6-src.zip
-set outstandalonezip=pyNsource-1.6-standalone.zip
+set outsourcezip=pyNsource-1.60-src.zip
+set outstandalonezip=pyNsource-1.60-standalone.zip
+set outsetupexe=pyNsource-1.60-setup.exe
 
-svn info > build.txt
+:regen_version_info_from_subversion
+svn info > version.txt
 
-REM goto step3a
+REM goto build_inno_setup_exe
+REM goto zip_source
 
-REM buildstandalone exe zip
+:delete_existing_dist_dir
+del /q dist\*.*
+
+:build_standalone_exe
 cd src
 del /q dist\*.*
 del /q build\*.*
-REM python setup.py py2exe
-bb-freeze pyNsourceGui.py
+bb-freeze pyNsourceGui.py pyYumlGui.py pynsource.py
 cd ..
+:zip_standalone_exe
+7z a -tzip  dist\%outstandalonezip% .\src\dist\* .\Readme.txt .\version.txt
 
-:step3a
-7z a -tzip  dist\%outstandalonezip% .\src\dist\* .\Readme.txt .\build.txt
+:zip_source
+7z a -tzip  -xr!".svn" -xr!"build" -xr!"dist" -xr!*.pyc dist\%outsourcezip% src\* tests\* Readme.txt build*.bat run*.bat version.txt
 
+:build_inno_setup_exe
+iscc buildwin_setupexe.iss
 
+:end
+dir dist
+echo All done!
