ff7f5d10be4ff60dbca46a5a5f05985f757232fa
abulka@gmail.com <abulka@gmail.com@6461a6f0-73df-cf69-53d8-f1e9cabed33a>
2012-08-28 11:27:45 +0000

    moved some very old pynsource manual test scripts into subdir

--- runasciiart.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM generate an asciiart uml diagram
-@echo on
-python src\pynsource.py -a tests\python-in\testmodule01.py > outasciiart.txt
-@echo off
-echo Copy your asci art UML into e.g Java Ascii Versatile Editor http://www.jave.de/
-notepad outasciiart.txt
