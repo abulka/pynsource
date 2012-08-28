4cc36220a8f25e3c76bd29e21d7b68d4519a168a
abulka@gmail.com <abulka@gmail.com@6461a6f0-73df-cf69-53d8-f1e9cabed33a>
2012-08-28 00:03:31 +0000

    right click menu cleanup - bind to exact method.

--- trunk/src/gui/uml_shape_handler.py
+++ trunk/src/gui/uml_shape_handler.py
@@ -94,66 +94,77 @@ class UmlShapeHandler(ogl.ShapeEvtHandler):
     def OnMovePost(self, dc, x, y, oldX, oldY, display):
         shape = self.GetShape()
         ogl.ShapeEvtHandler.OnMovePost(self, dc, x, y, oldX, oldY, display)
+        
         #self.UpdateStatusBar(shape)
+        
         #if "wxMac" in wx.PlatformInfo:
         #    shape.GetCanvas().Refresh(False) 
 
-    def OnPopupItemSelected(self, event):
-        item = self.popupmenu.FindItemById(event.GetId()) 
-        text = item.GetText() 
-        if text == "Delete\tDel":
-            self.RightClickDeleteNode()
-        elif text == "Properties...":
-            self.NodeProperties()
-        elif text == "Begin - Draw Line from this class\tq":
-            self.GetShape().GetCanvas().NewEdgeMarkFrom()
-        elif text == "End - Draw Line to this class (composition)\tw":
-            self.GetShape().GetCanvas().NewEdgeMarkTo(edge_type='composition')
-        elif text == "End - Draw Line to this class (generalisation)\tW":
-            self.GetShape().GetCanvas().NewEdgeMarkTo(edge_type='generalisation')
-        elif text == "Reset Image Size":
-            shape = self.GetShape()
-            shape.ResetSize()
-            #shape.GetCanvas().Refresh(False)   # don't seem to need since SelectNodeNow() might be doing it for us
-            shape.GetCanvas().SelectNodeNow(shape)
-            self.UpdateStatusBar(shape)
 
     def OnRightClick(self, x, y, keys, attachment):
         self._SelectNodeNow(x, y, keys, attachment)
-
         #self.log.WriteText("%s\n" % self.GetShape())
-        self.popupmenu = wx.Menu()     # Creating a menu
+
+        self.popupmenu = wx.Menu()
         
-        def MakeMenuItem(menu, msg):
-            item = menu.Append(wx.NewId(), msg)
-            self.frame.Bind(wx.EVT_MENU, self.OnPopupItemSelected, item)  # Not sure why but passing item is needed.  Not to find the menu item later, but to avoid crashes?  try two right click deletes followed by main menu edit/delete.  Official Bind 3rd parameter DOCO:  menu source - Sometimes the event originates from a different window than self, but you still want to catch it in self. (For example, a button event delivered to a frame.) By passing the source of the event, the event handling system is able to differentiate between the same event type from different controls.
+        def MakeMenuItem(parent_menu, msg, method):
+            """
+            Not sure why but passing item is needed to Bind. Not to find the
+            menu item later, but to avoid crashes? try two right click deletes
+            followed by main menu edit/delete. Official Bind 3rd parameter DOCO:
+            menu source - Sometimes the event originates from a different window
+            than self, but you still want to catch it in self. (For example, a
+            button event delivered to a frame.) By passing the source of the
+            event, the event handling system is able to differentiate between
+            the same event type from different controls. 
+            """
+            item = parent_menu.Append(wx.NewId(), msg)
+            self.frame.Bind(wx.EVT_MENU, method, item)
             
-        MakeMenuItem(self.popupmenu, "Properties...")
+        MakeMenuItem(self.popupmenu, "Properties...", self.NodeProperties)
         self.popupmenu.AppendSeparator()
 
         menu_sub = wx.Menu()
-        MakeMenuItem(menu_sub, "Begin - Draw Line from this class\tq")
-        MakeMenuItem(menu_sub, "End - Draw Line to this class (composition)\tw")
-        MakeMenuItem(menu_sub, "End - Draw Line to this class (generalisation)\tW")
+        MakeMenuItem(menu_sub, "Begin - Draw Line from this class\tq", self.OnDrawBegin)
+        MakeMenuItem(menu_sub, "End - Draw Line to this class (composition)\tw", self.OnDrawEnd1)
+        MakeMenuItem(menu_sub, "End - Draw Line to this class (generalisation)\tW", self.OnDrawEnd2)
         self.popupmenu.AppendMenu(wx.NewId(), "Draw Line", menu_sub)
 
         if self.GetShape().__class__.__name__ == 'BitmapShapeResizable':
             self.popupmenu.AppendSeparator()
-            MakeMenuItem(self.popupmenu, "Reset Image Size")
+            MakeMenuItem(self.popupmenu, "Reset Image Size", self.OnResetImageSize)
         
         self.popupmenu.AppendSeparator()
-        MakeMenuItem(self.popupmenu, "Delete\tDel")
+        MakeMenuItem(self.popupmenu, "Delete\tDel", self.RightClickDeleteNode)
         self.popupmenu.AppendSeparator()
-        MakeMenuItem(self.popupmenu, "Cancel")
+        MakeMenuItem(self.popupmenu, "Cancel", self.OnPopupMenuCancel)
         
         self.frame.PopupMenu(self.popupmenu, wx.Point(x,y))
 
-    def RightClickDeleteNode(self):
+    def RightClickDeleteNode(self, event):
         self.app.run.CmdNodeDelete(self.GetShape())
 
     def OnLeftDoubleClick(self, x, y, keys, attachment):
         self.app.run.CmdEditClass(self.GetShape())
 
-    def NodeProperties(self):
+    def NodeProperties(self, event):
         self.app.run.CmdEditClass(self.GetShape())
 
+    def OnDrawBegin(self, event):
+        self.GetShape().GetCanvas().NewEdgeMarkFrom()
+
+    def OnDrawEnd1(self, event):
+        self.GetShape().GetCanvas().NewEdgeMarkTo(edge_type='composition')
+
+    def OnDrawEnd2(self, event):
+        self.GetShape().GetCanvas().NewEdgeMarkTo(edge_type='generalisation')
+
+    def OnResetImageSize(self, event):
+        shape = self.GetShape()
+        shape.ResetSize()
+        #shape.GetCanvas().Refresh(False)   # don't seem to need since SelectNodeNow() might be doing it for us
+        shape.GetCanvas().SelectNodeNow(shape)
+        self.UpdateStatusBar(shape)
+        
+    def OnPopupMenuCancel(self, event):
+        pass
