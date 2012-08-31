a245e290f37ae4205f898a5fd042ce553314439e
abulka <abulka@6461a6f0-73df-cf69-53d8-f1e9cabed33a>
2012-08-31 05:17:17 +0000

    build output files names tweaked again
    Update issue 6
    build output files names tweaked again

--- buildwin.bat
+++ buildwin.bat
@@ -5,7 +5,6 @@ REM ensure "c:\Program Files (x86)\Inno Setup 5\" is in your path
 
 set outsourcezip=pyNsource-1.60-src.zip
 set outstandalonezip=pyNsource-1.60-win32-standalone.zip
-set outsetupexe=pyNsource-1.60-win32-setup.exe
 
 :regen_version_info_from_subversion
 svn info > version.txt
