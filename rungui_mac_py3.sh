be94e93eb5624c8bff9b3967494d7dc0692dfffc
Andy Bulka <abulka@gmail.com>
2019-01-09 12:05:07 +1100

    updated travis to python3

--- rungui_mac_py3.sh
+++ rungui_mac_py3.sh
@@ -1,7 +1,8 @@
 cd src
 
-#PYTHON=/usr/local/bin/python3
-PYTHON=/usr/local/Cellar/python/3.7.0/bin/python3
+PYTHON=python3
+# PYTHON=/usr/local/bin/python3
+# PYTHON=/usr/local/Cellar/python/3.7.0/bin/python3
 #PYTHON=/usr/local/Cellar/python/3.7.2/bin/python3
 
 $PYTHON pyNsourceGui.py
