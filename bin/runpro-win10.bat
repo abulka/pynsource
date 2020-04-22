73d9b3142c8d59d0d4827cc4577fffc9f8192130
Andy Bulka <abulka@gmail.com>
2020-04-22 16:39:19 +1000

    renamed runner scripts

---
+++ bin/runpro-win10.bat
@@ -0,0 +1,7 @@
+setlocal
+cd src
+REM set PYTHONPATH=\Users\Andy\ogl2;\Users\Andy\pynsource-rego
+set PYTHONPATH=\Users\Andy\Devel\ogl2;\Users\Andy\Devel\pynsource-rego
+rem exit /b
+python pynsource-gui.py
+endlocal
